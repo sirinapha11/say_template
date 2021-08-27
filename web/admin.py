8c9fed4f18be74de8f0bf3ca3830257e114814b7
film sirinapha <86333939+sirinapha11@users.noreply.github.com>
2021-08-27 10:02:13 +0700

    v

---
+++ web/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from web.models import Reporter, Article
+ 
+
+@admin.register(Reporter)
+class ReporterAdmin(admin.ModelAdmin):
+    list_display = ('email', 'first_name', 'last_name')
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['head_line', 'pub_date', 'reporter']
---
+++ web/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, HttpResponse
+from web.models import Article
+# Create your views here.
+
+
+def index (request):
+    context = {}
+    article_q = Article.objects.all()
+
+    context['articles'] = article_q
+
+    return render(request, 'index.html', context)
+    
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
